18fecbc37ecd1f569dcf17bdf8329de32ae68af8
Cassiano de Souza <redhotcassino@gmail.com>
2018-06-26 15:06:12 -0300

    Criando metodo de delete.

--- bills/index.js
+++ bills/index.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import create from './services/create'
+import remove from './services/remove'
 
 const router = express.Router()
 
 router.post('/', create)
+router.delete('/:id', remove)
 
 export default router
